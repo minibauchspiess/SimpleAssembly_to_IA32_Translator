8fc9db9022ec2645c6aa23f044a53767fd65ac7d
Minibauchspiess <dbauchspiess@gmail.com>
2020-11-28 19:40:07 -0300

    Estruturacao da funcao de traducao

---
+++ include/translateClass.h
@@ -0,0 +1,57 @@
+#pragma once
+
+
+#include <fstream>  //Bib para leitura e escrita de arquivos
+#include <map>
+
+#include "lineOperations.h"
+
+
+using namespace std;
+
+
+/*
+ *** Classe que traduz um arquivo na linguagem inventada assembly (.asm) para linguagem IA32 (.s) ***
+
+ * Recebe como argumento o nome do arquivo com o codigo assembly inventado
+ * 
+ *  - Tratamento de SECTION TEXT e SECTION DATA:
+ *      - Existem 3 strings auxiliares, utilizadas para armazenar o código traduzido que entram nas secoes .text, .data e .bss
+ *      - O codigo eh traduzido por completo para essas strings (selecionadas a depender da secao sendo lida)
+ *      - Apos traduzido, as 3 strings sao inseridas na ordem certa no arquivo de saida
+ *  - Tradução:
+ *      - Cada instrução no assembly inventado é mapeado a um conjunto de instruções em IA32 (comando switch)
+ * 
+ * 
+*/
+class translateClass
+{
+private:
+
+    //Arquivos manipulados
+    ifstream inputFile;  //Arquivo original assembly, .asm
+    ofstream outFile;     //Arquivo traduzido, .s
+
+
+    //Secoes do codigo (texto, dados e variaveis)
+    string secText;   //section .text do IA32 (escrita no arquivo .s)
+    string secData;   //section .data do IA32 (escrita no arquivo .s)
+    string secBss;    //section .bss do IA32 (escrita no arquivo .s)
+
+    //Flags de controle
+    int secFlag;    //Flag indicando a qual sessao o trecho traduzido pertence (0 para .data, 1 para .bss e 2 para .text)
+
+
+public:
+
+    translateClass(string inFileName);  //Abre os arquivos de entrada e saída, inicializa as strings de sessao
+    ~translateClass();
+
+    //Principal funcao. Le linha e chama ciclicamente a funcao de traducao de instrucao, e insere as linhas traduzidas na posicao correta do codigo de saida
+    string Run();
+
+    //Funcao que determina a traducao apropriada da linha, baseada na tabela de traducoes
+    void TranslateLine(string line);
+
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -7,43 +7,17 @@
 #include <map>
 #include <vector>
 
-#include "preprocessClass.h"
-#include "mountClass.h"
+#include "translateClass.h"
 
 using namespace std;
 
 
 int main(int argc, char* argv[]){
 
-    //Caso passe uma diretiva de compilacao (-p ou -o), executa somente a etapa requerida
-    if(argc==3){
-        string fileName(argv[2]);
+    string a = "um dois";
+    string b = ", feijao com arroz";
 
-        string ppCommand="-p", mComannd = "-o";
-
-        //Caso seja -p, apenas preprocessamento eh feito, fileName eh um .asm
-        if(argv[1] == ppCommand){
-            preprocessClass ppObj(fileName);
-            ppObj.Run();
-        }
-        //Caso seja -o, apenas montagem, filename eh um .pre
-        else if(argv[1] == mComannd){
-            mountClass mountObj(fileName);
-            mountObj.Run();
-        }
-    }
-    //Caso nao haja uma diretiva de compilacao, preprocessa e monta
-    else{
-        string fileName(argv[1]), ppFileName;
-
-        //Preprocessa
-        preprocessClass ppObj(fileName);
-        ppFileName = ppObj.Run();
-
-        //Monta
-        mountClass mountObj(ppFileName);
-        mountObj.Run();
-    }
+    cout<<a+b<<endl;
 
     return 1;
 }
